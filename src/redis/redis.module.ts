bd1fc6a0ca5da0f073534d2c1402ce07d9406002
chenchao <beachboy@yeah.net>
2023-12-19 17:01:17 +0800

    add .env config

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,25 +1,30 @@
 import { Module, Global } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { createClient } from 'redis';
-
+import { ConfigService } from '@nestjs/config';
 @Global()
 @Module({
   providers: [
     RedisService,
     {
       provide: 'REDIS_CLIENT',
-      async useFactory() {
+      async useFactory(configService: ConfigService) {
+        const host = configService.get('redis_server_host');
+        const port = configService.get('redis_server_port') * 1;
+        const database = configService.get('redis_server_db') * 1;
+        // console.log(host, port, database);
         const client = createClient({
           socket: {
-            host: 'localhost',
-            port: 6379,
+            host,
+            port,
           },
-          database: 1,
+          database,
         });
         await client.connect();
 
         return client;
       },
+      inject: [ConfigService],
     },
   ],
   exports: [RedisService],
